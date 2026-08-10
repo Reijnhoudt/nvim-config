ade53440b9003d65948f3ad050bbb80941ae11a4
Niels Reijnhoudt <Niels.Reijnhoudt@wur.nl>
2026-08-10 13:57:48 +0200

    File explorer now opens in the window instead of a sidebar.

--- lua/config/core.lua
+++ lua/config/core.lua
@@ -20,7 +20,7 @@ vim.o.foldexpr = "v:lua.vim.treesitter.foldexpr()"
 
 -- File Explorer Style
 vim.g.netrw_liststyle = 3
-vim.g.netrw_altfile = 0 -- Lets ctrl-^ ignore netrw when jumping between files
+vim.g.netrw_altfile = 1 -- Lets ctrl-^ ignore netrw when jumping between files
 
 -- ============================================================================
 -- Mappings
@@ -42,4 +42,4 @@ vim.keymap.set('n', '<leader>d', vim.diagnostic.open_float, { desc = 'Show diagn
 vim.keymap.set('i', '<C-Space>', '<C-x><C-o>', { desc = "Omnicomplete" })
 
 -- Open file explorer
-vim.keymap.set('n', '<S-e>', '<Cmd>Lexplore<CR>', { desc = 'Open file explorer (netrw)' })
+vim.keymap.set('n', '<S-e>', '<Cmd>Explore<CR>', { desc = 'Toggle file explorer' })
